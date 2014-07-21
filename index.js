580c53779354e564c9039e3141c76cac67e66ba2
Takuto Wada <takuto.wada@gmail.com>
2014-07-21 14:26:01 +0900

    test(escallmatch): spike solution

--- index.js
+++ index.js
@@ -1,9 +1,84 @@
-function Matcher (pattern) {
+var esprima = require('esprima'),
+    estraverse = require('estraverse'),
+    syntax = estraverse.Syntax,
+    keys = Object.keys || require('object-keys'),
+    //esprimaOptions = {tolerant: true, loc: true, tokens: true, raw: true},
+    esprimaOptions = {tolerant: true};
+
+function createMatcher (pattern) {
+    var ast = extractExpressionFrom(esprima.parse(pattern, esprimaOptions));
+    return new Matcher(ast);
 }
-Matcher.prototype.test = function () {
+
+function Matcher (exampleAst) {
+    this.rules = rules(exampleAst);
+    console.log(JSON.stringify(this.rules, null, 2));
+}
+Matcher.prototype.test = function (controller) {
+    var that = this;
+    return keys(that.rules).map(function (key) {
+        return that.rules[key];
+    }).some(function (val) {
+        val.parentNodes;
+    });
     return false;
 };
 
-module.exports = function (pattern) {
-    return new Matcher(pattern);
-};
+
+function rules (exampleAst) {
+    var pathToNode = {};
+    var rules = {};
+    estraverse.traverse(exampleAst, {
+        enter: function (currentNode, parentNode) {
+            var path = this.path(),
+                espath = path ? path.join('/') : '';
+            pathToNode[espath] = currentNode;
+        },
+        leave: function (currentNode, parentNode) {
+            var controller = this,
+                path = controller.path(),
+                espath = path ? path.join('/') : '';
+            if (currentNode.type === syntax.Identifier && startsWith(currentNode.name, '$')) {
+                // var nodes = [];
+                // collect(nodes, espath, pathToNode);
+                // nodes.shift();
+                rules[currentNode.name] = {
+                    espath: espath,
+                    currentNode: currentNode,
+                    parentNodes: controller.parents()
+                };
+            }
+        }
+    });
+    return rules;
+}
+
+function collect (acc, espath, pathToNode) {
+    var node = pathToNode[espath];
+    if (node) {
+        acc.push(node);
+    }
+    if (espath === '') {
+        return;
+    } else {
+        collect(acc, parentEspath(espath), pathToNode);
+    }
+}
+
+function parentEspath (espath) {
+    var elements = espath.split('/');
+    return elements.slice(0, elements.length - 1).join('/');
+}
+
+function startsWith (str, phrase) {
+    return str.lastIndexOf(phrase, 0) === 0;
+}
+
+function extractExpressionFrom (tree) {
+    var expressionStatement = tree.body[0],
+        expression = expressionStatement.expression;
+    return expression;
+}
+
+createMatcher.parentEspath = parentEspath;
+module.exports = createMatcher;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,26 @@
 var assert = require('assert'),
+    esprima = require('esprima'),
+    //esprimaOptions = {tolerant: true, loc: true, tokens: true, raw: true},
+    esprimaOptions = {tolerant: true},
+    estraverse = require('estraverse'),
     esexample = require('..');
 
+it('parentEspath', function () {
+    assert.equal(esexample.parentEspath('arguments/0/right'), 'arguments/0');
+    assert.equal(esexample.parentEspath('arguments/0'), 'arguments');
+    assert.equal(esexample.parentEspath('arguments'), '');
+    assert.equal(esexample.parentEspath(''), '');
+});
+
 it('generate matcher from given example', function () {
-    var matcher = esexample('assert.equal($actual, $expected)');
-    assert(matcher.test('assert.equal(foo, bar)'));
+    var matcher = esexample('assert($actual)');
+    var ast = esprima.parse('assert(foo)', esprimaOptions);
+    var collector = [];
+    estraverse.traverse(ast, {
+        leave: function (currentNode, parentNode) {
+            var controller = this;
+            collector.push(matcher.test(controller));
+        }
+    });
+    assert(collector.some(function (matched) { return matched; }));
 });
