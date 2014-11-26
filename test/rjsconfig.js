49642a9d167e3ce603f300d3e709760d5ce2b6f9
Takuto Wada <takuto.wada@gmail.com>
2014-11-26 18:00:25 +0900

    chore(escallmatch): rjsconfig does not need es5-shim

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -1,7 +1,6 @@
 var require = {
     paths: {
         escallmatch: "../build/escallmatch",
-        "es5-shim": "../bower_components/es5-shim/es5-shim",
         esprima: '../bower_components/esprima/esprima',
         estraverse: '../bower_components/estraverse/estraverse',
         espurify: '../bower_components/espurify/build/espurify',
