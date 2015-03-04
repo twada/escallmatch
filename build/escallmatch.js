e7a3c932083377a6bb5c23a0e7f01b566df74103
Takuto Wada <takuto.wada@gmail.com>
2015-03-04 23:59:37 +0900

    chore(package): update array-foreach to 1.0.1

--- build/escallmatch.js
+++ build/escallmatch.js
@@ -244,12 +244,6 @@ module.exports = function forEach (ary, callback, thisArg) {
         ary.forEach(callback, thisArg);
         return;
     }
-    if (typeof ary === 'undefined' || ary === null) {
-        throw new TypeError();
-    }
-    if (typeof callback !== 'function') {
-        throw new TypeError();
-    }
     for (var i = 0; i < ary.length; i+=1) {
         callback.call(thisArg, ary[i], i, ary);
     }
